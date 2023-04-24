8f0581780501a21440480d35d7ffaf78e4b916b0
Anicet Ebou <anicet.ebou@gmail.com>
2023-04-24 18:53:16 +0000

    fix: various fixes

--- R/get_core.R
+++ R/get_core.R
@@ -2,8 +2,6 @@
 #'
 #' @param x A data frame representing a ASV/OTU table with raw counts or
 #'          phyloseq object.
-#' @param taxa_are_rows A single logical specifying the orientation of
-#'                      the abundance table.
 #'
 #' @return A data frame of species abundance distribution of core microbiome.
 #'
@@ -12,7 +10,7 @@
 #' get_core(df)
 #'
 #' @export
-get_core <- function(x, taxa_are_rows=FALSE) {
-    xsad <- sad(x, taxa_are_rows=taxa_are_rows)
+get_core <- function(x) {
+    xsad <- sad(x)
     return(xsad[xsad$type=="core", ])
 }
--- R/get_satellite.R
+++ R/get_satellite.R
@@ -2,8 +2,6 @@
 #'
 #' @param x A data frame representing a ASV/OTU table with raw counts or
 #'          phyloseq object.
-#' @param taxa_are_rows A single logical specifying the orientation of
-#'                      the abundance table.
 #'
 #' @return A data frame of species abundance distribution of satellite species
 #'
@@ -12,7 +10,7 @@
 #' get_satellite(df)
 #'
 #' @export
-get_satellite <- function(x, taxa_are_rows = FALSE) {
-    xsad <- sad(x, taxa_are_rows = taxa_are_rows)
+get_satellite <- function(x) {
+    xsad <- sad(x)
     return(xsad[xsad$type == "satellite", ])
 }
